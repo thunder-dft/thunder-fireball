29a1aa918331c099bd3c619eeeac3d051e426b88
psience-guy <james.p.lewis.phd@gmail.com>
2023-08-01 13:02:49 +0800

    MPI path
    Added f.MPI path

--- src/runme_before_compile.sh
+++ src/runme_before_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MASTER_PATH=thunder-master
 
-for i in include Makefile MACHINES a.GLOBAL b.FUNCTIONS c.SYSTEM e.FDATA g.XC_FUNCTIONALS h.SOLVESH i.GRID j.ASSEMBLERS k.DASSEMBLERS l.SCF m.MD o.OUTPUT u.UTIL
+for i in include Makefile MACHINES a.GLOBAL b.FUNCTIONS c.SYSTEM e.FDATA f.MPI g.XC_FUNCTIONALS h.SOLVESH i.GRID j.ASSEMBLERS k.DASSEMBLERS l.SCF m.MD o.OUTPUT u.UTIL
 do
     if [ -h $i ]
     then
